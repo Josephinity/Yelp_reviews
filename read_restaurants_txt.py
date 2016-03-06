63b11b9388cf3975a17f57e60283140aeabc35a9
Jo Hu <jojocat1010@gmail.com>
2016-03-06 10:49:47 -0800

    201603
    Done with rf and nb

--- read_restaurants_txt.py
+++ read_restaurants_txt.py
@@ -1,8 +1,17 @@
 import pandas as pd
-def read_restaurants_txt(filename='restaurants_0_50.txt',seperator='\\'):
+def read_restaurants_txt(filename='restaurants_0_50.txt',seperator='#'):
     with open(filename) as f:
-        lines=[[line] for line in f.readlines()]
-        header=lines[0][0].split(seperator)[1:]
+        lines=[line for line in f.readlines() if line]
+        header=lines[0].split(seperator)[1:]
         header[-1]=header[-1][:-1]
-        content=[line[0].split(seperator)[1:] for line in lines[1:]]
+        content=[]
+        current=''
+        for line in lines[1:]:
+            if len(line)>3 and line[-3]=='#': 
+                current+=line
+                content.append(current)
+                current=''
+            elif line is not '':
+                current+=line
+        content=[line[:-1].replace('"','').split(seperator)[1:] for line in content]
     return pd.DataFrame(content,columns=header)
